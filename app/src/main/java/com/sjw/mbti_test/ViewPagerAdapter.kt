3eb514103ffada81da42ad3a550a3a0b6ba5a21c
shinjiwon79227 <shinjiwon79227@gmail.com>
2024-05-30 16:44:40 +0900

    Initial commit

---
+++ app/src/main/java/com/sjw/mbti_test/ViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.sjw.mbti_test
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return QuestionFragment.newInstance(position)
+    }
+}
